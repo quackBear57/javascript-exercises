5ce1daf6dd4fce166dbdced3e8773257c51841cc
quackBear57 <89419185+quackBear57@users.noreply.github.com>
2025-02-05 09:11:35 -0600

    04 solution and full test file

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...args) {
+    for (let arg of args) {
+        let i = 0;
+        while (i < inputArray.length) {
+            if (inputArray[i] === arg) {
+                inputArray.splice(i, 1);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    return inputArray;
 };
 
 // Do not edit below this line
