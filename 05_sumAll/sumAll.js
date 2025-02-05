83072f66c4bdfb5edf018482612de304536bbb6b
quackBear57 <89419185+quackBear57@users.noreply.github.com>
2025-02-05 09:23:04 -0600

    05 solution, full test file

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(sumStart, sumEnd) {
+    if (sumStart < 0 || sumEnd < 0 ||
+        !Number.isInteger(sumStart) || !Number.isInteger(sumEnd)) {
+        return "ERROR";
+    }
+    
+    let sumOutput = 0;
+    for (let i = Math.min(sumStart,sumEnd);
+        i <= Math.max(sumStart, sumEnd);
+         i++) {
+        sumOutput += i;
+    }
+    return sumOutput;
 };
 
 // Do not edit below this line
