65f602f5f035145be8f841d7c63433733b58260f
quackBear57 <89419185+quackBear57@users.noreply.github.com>
2025-02-04 19:23:23 -0600

    04 solution and full test file

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let outputString = "";
+    for (let i = inputString.length - 1; i >= 0; i--) {
+        outputString += inputString.substr(i,1);
+    }
+    return outputString;
 };
+
+console.log(reverseString("hello"))
 
 // Do not edit below this line
 module.exports = reverseString;
