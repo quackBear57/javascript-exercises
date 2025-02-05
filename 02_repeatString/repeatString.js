a5a3db9d1e32e70a2638e1202c06604e9f7154e8
quackBear57 <89419185+quackBear57@users.noreply.github.com>
2025-02-04 19:09:40 -0600

    solution to 02 and full test file

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(stringInput, numberToRepeat) {
+    if (numberToRepeat < 0) {
+        return "ERROR";
+    }
+    
+    let stringOutput = "";
+    for (let i = 1; i <= numberToRepeat; i++) {
+        stringOutput += stringInput;
+    }
+    return stringOutput;
 };
 
 // Do not edit below this line
