19432bb79c22933cfb78d82e995c2726023ce580
quackBear57 <89419185+quackBear57@users.noreply.github.com>
2025-02-07 11:13:32 -0600

    finished solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (stringToCheck) {
+    let cleanerString = stringToCheck.toLowerCase().trim();
+    let cleanestString = cleanerString.replace(/[^\w\s\']|_/g, "").replace(/\s/g, '');
 
+    let reversedString = "";
+    for (let i = cleanestString.length - 1; i >= 0; i--){
+        reversedString += cleanestString.substr(i,1);
+    };
+    return (reversedString === cleanestString);
 };
 
 // Do not edit below this line
