9701a1f9000f8dc7082eac077d62a0380cc201b7
quackBear57 <89419185+quackBear57@users.noreply.github.com>
2025-02-08 16:18:42 -0600

    solution and full test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let convertedNum = parseInt(num);
 
+    if (convertedNum === 0) {
+        return 0;
+    } else if (convertedNum < 0) {
+        return "OOPS";
+    } else {
+        let firstNum = 0;
+        let secNum = 1;
+        let nextNum = 1;
+        for (let i = 1; i < convertedNum; i++){
+            nextNum = firstNum + secNum;
+            firstNum = secNum;
+            secNum = nextNum;
+        };
+        return nextNum;
+    }
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
