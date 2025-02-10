bf3273ea33c8fb9cc6a4b9e239a092e3796d36d6
quackBear57 <89419185+quackBear57@users.noreply.github.com>
2025-02-10 14:23:17 -0600

    full solution and tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.forEach((person) => {
+        if(!("yearOfDeath" in person)) {
+            const d = new Date();
+            const year = d.getFullYear();
+            person.yearOfDeath = year;
+        }
+    });
+    
+    const oldest = people.sort(function(firstPerson, secondPerson) {
+        const lastGuy = firstPerson.yearOfDeath - firstPerson.yearOfBirth;
+        const nextGuy = secondPerson.yearOfDeath - secondPerson.yearOfBirth;
+        return lastGuy > nextGuy ? -1 : 1;
+        });
+    return oldest[0];
 };
 
 // Do not edit below this line
