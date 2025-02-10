ccff6e32d77481edcc4c9f66941b53bb4b7a80f5
quackBear57 <89419185+quackBear57@users.noreply.github.com>
2025-02-10 13:34:46 -0600

    full solution

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titles = [];
+    books.forEach((item) => {
+        titles.push(item.title);
+    });
+    return titles;
 };
 
 // Do not edit below this line
